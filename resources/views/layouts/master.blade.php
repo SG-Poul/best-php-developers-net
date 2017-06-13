3cc1416ac2da04c0d2900f8b57f273ef2165b9b7
OleksandrSGr <oleksandr.sgr@gmail.com>
2017-06-13 11:37:45 +0300

    add header WIP

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,73 +1,81 @@
 
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <style>
-html, body {
-    background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-    height: 100vh;
-        }
-
-        .flex-center {
-    align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-    position: relative;
-}
-
-        .top-right {
-    position: absolute;
-    right: 10px;
-            top: 18px;
-        }
-
-        .content {
-    text-align: center;
-        }
-
-        .title {
-    font-size: 84px;
-        }
-
-        .links > a {
-    color: #636b6f;
-    padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-    margin-bottom: 30px;
-        }
-    </style>
+    <title>PHP Developers</title>
+    <link href="/css/app.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
+<div id="top-header-div">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6">
+                <div id="header-logo-div">
+                    <a href="/">
+                        <img src="/img/header-logo.png">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <div id="header-navbar">
+                    <a href="/">Home</a>
+                    <a href="/">Prices</a>
+                    <a href="/">Projects</a>
+                    <a href="/">Photos</a>
+                    <a href="/">Contacts</a>
+                    <a href="/">Request free quote</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="nav-bar-main">
+    <div class="container">
+        <div class="row">
+            {{--<div class="col-xs-6">--}}
+                <table id="phone-table">
+                    <tr>
+                        <td rowspan="3"><img src="/img/ph.png"></td>
+                        <td> USA  </td>
+                        <td> +1-929-999-1235 </td>
+                        <td rowspan="3"><img src="/img/sk.png"></td>
+                        <td rowspan="3">bestdevelopers</td>
+                        <td rowspan="3"> </td>
+                        <td rowspan="3"> <a href="#">PHP Development</a></td>
+                        <td rowspan="3"> | </td>
+                        <td rowspan="3"> <a href="#">Hire PHP Developers</a></td>
+                        <td rowspan="3"> | </td>
+                        <td rowspan="3"> <a href="#">Dedicated PHP Team</a></td>
+                        <td rowspan="3"> | </td>
+                        <td rowspan="3"> <a href="#">Company</a></td>
+                    </tr>
+                    <tr>
+                        <td> DEU </td>
+                        <td> +44-203-514-5635 </td>
+                    </tr>
+                    <tr>
+                        <td> UKR </td>
+                        <td> +38-056-794-0298 </td>
+                    </tr>
+                </table>
+            {{--</div>--}}
+            {{--<div class="col-xs-6">--}}
+
+
+
+            {{--</div>--}}
+        </div>
+    </div>
+</div>
+
 @yield('content');
 
 @yield('script');
+
 </body>
 </html>
